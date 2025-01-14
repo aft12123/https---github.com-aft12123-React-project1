7a9c21f7cef1a015f7863e2412c059c32b493074
aft12123 <aqazi6260@gmail.com>
2025-01-14 23:57:30 +0530

    REACT

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 
 import Home from './view/Home';
+import Navbar from './view/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-  <Home/>
+      <Navbar/>
+      <Home/>
           
     
     
--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,53 +1,18 @@
 import React from "react";
 import './Home.css';
-import i6 from '../Accetes/i.6.jpg';
 import i1 from '../Accetes/i.1.jpg';
 import i4 from '../Accetes/i.4.jpg';
 import i5 from '../Accetes/i.5.jpg';
 import i2 from '../Accetes/i.2.jpg';
+import KARES from'../view/KARES.jpg';
 const Home=()=>{
     return(
-        <div>
-          <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    <div>
+      <div>
+        <img src={KARES} className="KARES"/>
     </div>
-  </div>
-</nav>
-         
-          
-       <div className="header"/>
+   <div className="header"/>
   <div className="img"/>
-    <img src={i6}className="i6"/>
   <div>
     <h1 className="h1">Welcome to KA Resturent</h1>
 </div>
@@ -68,9 +33,12 @@ const Home=()=>{
 <div  className="menu">
     <h2>Our Menu</h2>
 </div>
+  <div>
+
+</div>
     <div className="menu-items">
       <div className="item">
-        <img src={i1}className="i1"/>
+      <img src={i1}className="i1"/>
         <p>BREAKFAST</p>
       </div>
       <div className="item">
@@ -88,8 +56,12 @@ const Home=()=>{
     </div>
   
 <div/>
-    <p className="KA">2024 KA Resturent</p>
-    <p className="Contact"> contact us at 7676071869</p>
+<div>
+<p className="AK">2024 KA Resturent</p>
+<p className="Cont">contact us at 7676071869</p>
+
+</div>
+    
 </div>
 )
 
